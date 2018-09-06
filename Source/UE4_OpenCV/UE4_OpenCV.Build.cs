fcd7f0ec04b95d16440c06c3d09622f604eb5f53
Jakob Weiss <jakob.weiss@tum.de>
2018-09-06 16:59:10 +0200

    Not sure what I was doing anymore, but currently does not work.
    Intention was to render directly to a render target.. apparently did not
    work out that way. Investigate further, there should be a way..

--- Source/UE4_OpenCV/UE4_OpenCV.Build.cs
+++ Source/UE4_OpenCV/UE4_OpenCV.Build.cs
@@ -8,16 +8,22 @@ public class UE4_OpenCV : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OpenCV" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                "OpenCV/Private",
+			}
+            );
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
