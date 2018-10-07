fe2807ccb366a128f601d118714202ee058c92b1
Jakob Weiss <jakob.weiss@tum.de>
2018-10-07 19:13:35 +0200

    Build And Header cleanup - now should package properly

--- Source/UE4_OpenCV/UE4_OpenCV.Build.cs
+++ Source/UE4_OpenCV/UE4_OpenCV.Build.cs
@@ -7,23 +7,17 @@ public class UE4_OpenCV : ModuleRules
 	public UE4_OpenCV(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OpenCV" });
+
+        bEnableExceptions = true;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OpenCV" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
         PrivateIncludePaths.AddRange(
             new string[] {
-                "OpenCV/Private",
+                "OpenCV/Public",
 			}
             );
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 }
